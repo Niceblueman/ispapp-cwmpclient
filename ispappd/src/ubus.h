d598200b710b705d9a836f1a6994e4419c0dfae6
KM8Oz <ouazmir@gmail.com>
2025-07-10 21:50:00 +0100

    fix: Remove outdated license information from ubus.h

--- ispappd/src/ubus.h
+++ ispappd/src/ubus.h
@@ -1,11 +1,3 @@
-/*
- *	This program is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 2 of the License, or
- *	(at your option) any later version.
- *
- *	Copyright (C) 2012 Luka Perkov <freecwmp@lukaperkov.net>
- */
 
 #ifndef _ISPAPPCWMP_UBUS_H__
 #define _ISPAPPCWMP_UBUS_H__
